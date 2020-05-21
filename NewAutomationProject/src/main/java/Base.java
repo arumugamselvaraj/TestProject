f9583df64141311ac92cb4938c65399592c3d9e1
Arumugam <Arumugam@Lenovo>
2020-05-22 00:59:27 +0530

    New Base Committ

--- NewAutomationProject/src/main/java/Base.java
+++ NewAutomationProject/src/main/java/Base.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -9,14 +12,20 @@ public class Base {
 	
 public WebDriver driver=null;
 public String baseurl="http://demo.guru99.com/test/newtours/reservation.php";
+
+
 @BeforeTest
-public void LaunchBrowswer()
+@Parameters({"browser"})
+public void LaunchBrowswer(String browser)
 {
+	if(browser.equals("chrome"))
+	{
 	WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 	driver.get(baseurl);
 }
-
+	
+}
 @AfterTest
 public void terminateBrowser(){
     driver.close();
