dfbe033c52c5bca8bd81a838d1e47bb70a83505c
Arumugam <Arumugam@Lenovo>
2020-06-02 20:25:24 +0530

    ymail send email committ

--- NewAutomationProject/src/test/java/gmail_login/tests/MailLogin.java
+++ NewAutomationProject/src/test/java/gmail_login/tests/MailLogin.java
@@ -16,7 +16,12 @@ public class MailLogin extends Base {
 		driver.findElement(By.id("login-passwd")).sendKeys("8754604222");
 		driver.findElement(By.id("login-signin")).click();
 		driver.findElement(By.linkText("Compose")).click();
-			 }
+		driver.findElement(By.xpath("//*[@id=\"message-to-field\"]")).sendKeys("arubtech@yahoo.com");
+		driver.findElement(By.xpath("//*[@id=\"mail-app-component\"]/div/div/div[1]/div[3]/div/div/input")).sendKeys("Selenium Test");
+	driver.findElement(By.xpath("//*[@id=\"mail-app-component\"]/div/div/div[2]/div[2]/div/button")).click();		
+	driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/div[3]/div[1]/div/div/div/a[3]/span[2]")).click();		
+	
+	}
 		
 	}
 
